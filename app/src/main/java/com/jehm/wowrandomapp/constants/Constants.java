73a364ead3d176babdaca438f8bfcf36e3d4a34f
JEHM94 <jesus.e.hamel@gmail.com>
2022-05-24 23:34:18 -0400

    Some updates

--- app/src/main/java/com/jehm/wowrandomapp/constants/Constants.java
+++ app/src/main/java/com/jehm/wowrandomapp/constants/Constants.java
@@ -1,8 +1,10 @@
 package com.jehm.wowrandomapp.constants;
 
+import com.jehm.wowrandomapp.BuildConfig;
+
 public class Constants {
-    public static final String CLIENT_ID = "fa75ca71e373400b9ed4c4c8c8c8c5b4";
-    public static final String CLIENT_SECRET = "xUEWjNdGp2ERMt6UI3UDlIvc7rwPCf7J";
+    public static final String CLIENT_ID = BuildConfig.CLIENT_ID;
+    public static final String CLIENT_SECRET = BuildConfig.CLIENT_SECRET;
     public static final String GRANT_TYPE = "client_credentials";
     public static final String LOGIN_STATE = "fs390a38jf9001p";
 
